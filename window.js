6869422a5cc3e25457ed41ad3b87ec6e9bc63ca3
Shivam Sharma <sdsharma@ucsd.edu>
2017-02-09 23:42:51 -0800

    Initial push

---
+++ window.js
@@ -0,0 +1,48 @@
+/*
+  Adapted from
+  http://github.com/scheib/HTMLMisc/blob/gh-pages/KeyboardState.html and
+  http://www.cryer.co.uk/resources/javascript/script20_respond_to_keypress.htm
+*/
+"use strict";
+
+var keys = '';
+
+function GetDescriptionFor(e)
+{
+  var result, code;
+  if ((e.charCode) && (e.keyCode==0))
+  {
+    code = e.charCode;
+  } else {
+    code = e.keyCode;
+  }
+  if ((code >= 41) && (code <=126)) result = String.fromCharCode(code);
+
+  return result;
+}
+function MonitorKey(e)
+{
+  if (!e) e=window.event;
+  var d = GetDescriptionFor(e);
+  if(d != '?'){
+    if(d != undefined){
+      keys += d;
+    }
+  }
+  else{
+    keys = keys.slice(3);
+    keys = keys.substring(0, keys.length - 25);
+    keys = 'A' + keys;
+    document.getElementById("scan-code").value = keys;
+    console.log(document.getElementById("scan-code").value);
+    // document.getElementById("scan-submit").click();
+    keys = '';
+  }
+  return true;
+}
+document.getElementById("scan-code").onkeypress = function(e) {
+    var chr = String.fromCharCode(e.which);
+    if ("?".indexOf(chr) >= 0)
+        return false;
+};
+document.addEventListener('keypress', MonitorKey, true);
